1b7ec3c709270b7eb7a8397e62ebb45fd27899e2
Ankit Bisen <76652297+ankitbisen12@users.noreply.github.com>
2022-12-28 23:48:31 +0530

    Adding TopologicalSort logic

---
+++ TopologicalSort.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+void topoSort(int node,vector<int>&visited,stack<int>&s, unordered_map <int, list <int>>&adj){
+    visited[node]=1;
+    
+    for(auto neighbour:adj[node]){
+        if(!visited[neighbour]){
+            topoSort(neighbour,visited,s,adj);
+        }
+    }
+    s.push(node);
+}
+vector<int> topologicalSort(vector<vector<int>> &edges, int v, int e)  {
+    // Write your code here
+    unordered_map<int,list<int>> adj;
+    for(int i=0;i<e;i++){
+         int u=edges[i][0];
+         int v=edges[i][1];
+        
+         adj[u].push_back(v);
+    }
+    
+    //call dfs topological sort for all components.
+    vector<int> visited(v);
+    stack<int> s;
+    for(int i=0;i<v;i++){
+        if(!visited[i]){
+           topoSort(i,visited,s,adj);
+        }
+    }
+    
+    vector<int> ans;
+    
+    while(!s.empty()){
+           ans.push_back(s.top());
+           s.pop();
+    }
+   
+    return ans;
+}
+
+
+int main(){
+
+
+    return 0;
+}
